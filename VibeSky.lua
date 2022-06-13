95ef4ea498764eea11102a9e2977759920a7ce84
codernocook <90613458+codernocook@users.noreply.github.com>
2022-06-13 14:54:05 +0700

    Create VibeSky.lua

---
+++ VibeSky.lua
@@ -0,0 +1,66 @@
+local Lighting = game.Lightning
+local random = math.random(100000000, 999999999)
+Lighting.Name = "Lighting"..random
+local LightingName = "Lighting"..random
+for i,v in pairs(LightingName:GetChildren()) do
+	v:Destroy()
+end
+wait(.1)
+---Instance---
+local Atmosphere = Instance.new("Atmosphere")
+local Sky = Instance.new("Sky")
+local Bloom = Instance.new("BloomEffect")
+local Blur = Instance.new("BlurEffect")
+local ColorCorrection = Instance.new("ColorCorrectionEffect")
+local DepthOfField = Instance.new("DepthOfFieldEffect")
+local SunRays = Instance.new("SunRaysEffect")
+--------------
+
+--Parent--
+Atmosphere.Parent = game[LightingName]
+Sky.Parent = game[LightingName]
+Bloom.Parent = game[LightingName]
+Blur.Parent = game[LightingName]
+ColorCorrection.Parent = game[LightingName]
+DepthOfField.Parent = game[LightingName]
+SunRays.Parent = game[LightingName]
+----------
+
+--------Vibe Sky pack--------
+	--Vibe Sky Pack
+	game[LightingName].Sky.SkyboxBk = "rbxassetid://5084575798"
+	game[LightingName].Sky.SkyboxDn = "rbxassetid://5084575916"
+	game[LightingName].Sky.SkyboxFt = "rbxassetid://5103949679"
+	game[LightingName].Sky.SkyboxLf = "rbxassetid://5103948542"
+	game[LightingName].Sky.SkyboxRt = "rbxassetid://5103948784"
+	game[LightingName].Sky.SkyboxUp = "rbxassetid://5084576400"
+	game[LightingName].Sky.MoonAngularSize = 0
+	game[LightingName].Sky.SunAngularSize = 0
+	game[LightingName].Brightness = 0
+	game[LightingName].GlobalShadows = true
+	game[LightingName].ClockTime = 17.8
+	game[LightingName].GeographicLatitude = 0
+
+
+	game[LightingName].Atmosphere.Density = 0.3
+	game[LightingName].Atmosphere.Offset = 0.25
+	game[LightingName].Atmosphere.Color = Color3.new(199, 199, 199)
+	game[LightingName].Atmosphere.Decay = Color3.new(106, 112, 125)
+	game[LightingName].Atmosphere.Glare = 0
+	game[LightingName].Atmosphere.Haze = 0
+
+	game[LightingName].Bloom.Enabled = true
+	game[LightingName].Bloom.Intensity = 1
+	game[LightingName].Bloom.Size = 24
+	game[LightingName].Bloom.Threshold = 2
+
+	game[LightingName].DepthOfField.Enabled = false
+	game[LightingName].DepthOfField.FarIntensity = 0.1
+	game[LightingName].DepthOfField.FocusDistance = 0.05
+	game[LightingName].DepthOfField.InFocusRadius = 30
+	game[LightingName].DepthOfField.NearIntensity = 0.75
+
+	game[LightingName].SunRays.Enabled = true
+	game[LightingName].SunRays.Intensity = 0.01
+	game[LightingName].SunRays.Spread = 0.1
+---------------------------------
